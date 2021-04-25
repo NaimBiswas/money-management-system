04731274992321ff77766e28ab4d37052d8776d1
NaimBiswas <nayeembiswas2@gmail.com>
2021-04-25 15:10:30 +0600

    Add body parser and runing with mongoose server

--- app.js
+++ app.js
@@ -4,12 +4,18 @@ const port = process.env.PORT || 3001
 const morgan = require("morgan")
 const cors = require("cors")
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+
+
 
 app.use(bodyParser.json())
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors());
 app.use(morgan("dev"));
+
+
+
 
 app.get('/', (req, res) => {
    res.send('Hello World!')
@@ -20,12 +26,21 @@ app.get('/', (req, res) => {
 
 
 
+app.listen(port, () => {
+   console.log(`Example app listening on port ${port}! `)
 
 
+   // Connect MongoDB at default port 27017.
+   mongoose.connect('mongodb://localhost:27017money-managment', {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true
+   }, (err) => {
+      if (!err) {
+         console.log('MongoDB Connection Succeeded.')
+      } else {
+         console.log('Error in DB connection: ' + err)
+      }
+   });
 
-
-
-
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}! `))
+})
