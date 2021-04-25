6da045f43af04f69e63fe3585aee21ae28fdcef2
NaimBiswas <nayeembiswas2@gmail.com>
2021-04-25 15:00:17 +0600

    required some dependencis

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3001
 const morgan = require("morgan")
+const cors = require("cors")
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors());
 app.use(morgan("dev"));
 
 app.get('/', (req, res) => {
