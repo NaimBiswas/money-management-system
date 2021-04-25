b062cf447cabbbc2dfb4f331588ee78200525adc
NaimBiswas <nayeembiswas2@gmail.com>
2021-04-25 14:56:24 +0600

    added morgan package

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3001
+const morgan = require("morgan")
+app.use(morgan("dev"));
 
 app.get('/', (req, res) => {
    res.send('Hello World!')
